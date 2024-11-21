b42ff4436dc850a9f4351bf1d6ac221d90de13df
jlc00052 <jlc00052@red.ujaen.es>
2024-11-21 16:49:44 +0100

    Drag and drop implementation for desktop in progress

--- src/windows/desktop/rightContent/collections/CollectionsList.tsx
+++ src/windows/desktop/rightContent/collections/CollectionsList.tsx
@@ -1,35 +1,67 @@
 import { SeriesData } from "@interfaces/SeriesData";
 import { RootState } from "@redux/store";
-import { useSelector } from "react-redux";
-import Card from "../Card";
+import { useSelector, useDispatch } from "react-redux";
+import { updateLibrary } from "@redux/slices/dataSlice"; // Acción para actualizar la librería
 import "./CollectionsList.scss";
-import { Suspense } from "react";
-import Loading from "@components/utils/Loading";
+import Card from "../Card";
 
-/**
- * A component that displays a list of series.
- *
- * Utilizes Redux to access the selected library and series from the global state.
- * It renders a series container with a list of series
- * from the selected library, each represented by a Card component.
- *
- * @returns A JSX element representing a series list.
- */
 function CollectionsList() {
-	const selectedLibrary = useSelector(
-		(state: RootState) => state.data.selectedLibrary
-	);
+  const dispatch = useDispatch();
+  const selectedLibrary = useSelector(
+    (state: RootState) => state.data.selectedLibrary
+  );
 
-	return (
-		<Suspense fallback={<Loading />}>
-			<div className="collection-container scroll" id="scroll">
-				{selectedLibrary &&
-					selectedLibrary.series.map((show: SeriesData) => (
-						<Card key={show.id} show={show} type="default" />
-					))}
-			</div>
-		</Suspense>
-	);
+  const handleDragStart = (index: number) => {
+    const dragItem = document.querySelectorAll(".card")[index];
+    dragItem?.classList.add("dragging");
+  };
+
+  const handleDragEnd = (index: number) => {
+    const dragItem = document.querySelectorAll(".card")[index];
+    dragItem?.classList.remove("dragging");
+  };
+
+  const handleDragOver = (index: number, e: React.DragEvent) => {
+    e.preventDefault();
+    const draggingElement = document.querySelector(".dragging");
+
+    if (!draggingElement) return;
+
+    const overElement = document.querySelectorAll(".card")[index];
+    if (overElement && overElement !== draggingElement) {
+      // Obtener el índice de los elementos arrastrados
+      const updatedSeries = [...(selectedLibrary?.series || [])];
+      const draggingIndex = Array.from(draggingElement.parentNode?.children || []).indexOf(draggingElement);
+      const [removedItem] = updatedSeries.splice(draggingIndex, 1);
+      updatedSeries.splice(index, 0, removedItem);
+
+      // Crear la librería actualizada
+      const updatedLibrary = {
+        ...selectedLibrary,
+        series: updatedSeries,
+      };
+
+      // Actualizar en Redux
+      dispatch(updateLibrary(updatedLibrary));
+    }
+  };
+
+  return (
+    <div className="collection-container scroll" id="scroll">
+      {selectedLibrary?.series.map((show: SeriesData, index: number) => (
+        <div
+          className="card"
+          key={show.id}
+          draggable
+          onDragStart={() => handleDragStart(index)}
+          onDragEnd={() => handleDragEnd(index)}
+          onDragOver={(e) => handleDragOver(index, e)}
+        >
+          <Card show={show} type="default" />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default CollectionsList;
